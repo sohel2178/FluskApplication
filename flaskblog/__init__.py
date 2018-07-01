ee7ec037b987609a9859cd01883b6c1eba7f199c
sohel2178 <sohel.ahmed2178@gmail.com>
2018-07-01 17:44:53 +0600

    Todays Commit

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 
 app = Flask(__name__)
@@ -12,5 +14,13 @@ bcrypt = Bcrypt(app)
 loginManager = LoginManager(app)
 loginManager.login_view = 'login'
 loginManager.login_message_category='info'
+
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+
+mail = Mail(app)
 
 from flaskblog import routes
--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from flaskblog import db,loginManager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flaskblog import db,loginManager,app
 from flask_login import UserMixin
 
 @loginManager.user_loader
@@ -14,8 +15,25 @@ class User(db.Model,UserMixin):
     password = db.Column(db.String(60),nullable=False)
     posts = db.relationship('Post',backref='author',lazy=True)
 
+    def get_reset_token(self,expires_sec=1800):
+        s = Serializer(app.config['SECRET_KEY'],expires_sec)
+        return s.dumps({'user_id':self.id}).decode('utf-8')
+
+    @staticmethod
+    def varify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        
+        return User.query.get(user_id)
+        
+        
+
     def __repr__(self):
-            return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 class Post(db.Model):
     id = db.Column(db.Integer,primary_key=True)
