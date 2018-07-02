a0ea28506d414c4ec2e2ef99bad5794f8037320c
sohel2178 <sohel.ahmed2178@gmail.com>
2018-07-02 19:53:37 +0600

    2-july-2018

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,26 +1,52 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from flaskblog.config import Config
 
 
-app = Flask(__name__)
-app.config['SECRET_KEY']='a9037c898f658e8d4b72d540d8490d82'
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-loginManager = LoginManager(app)
-loginManager.login_view = 'login'
+
+# app.config['SECRET_KEY']= 'a9037c898f658e8d4b72d540d8490d82'
+# app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///site.db'
+# app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+# app.config['MAIL_PORT'] = 587
+# app.config['MAIL_USE_TLS'] = True
+# app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+# app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+
+
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+loginManager = LoginManager()
+loginManager.login_view = 'users.login'
 loginManager.login_message_category='info'
 
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+mail = Mail()
 
-mail = Mail(app)
 
-from flaskblog import routes
+
+#from flaskblog import routes
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    loginManager.init_app(app)
+    mail.init_app(app)
+
+    from flaskblog.users.routes import users
+    from flaskblog.posts.routes import posts
+    from flaskblog.main.routes import main
+    from flaskblog.errors.handlers import errors
+
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+    app.register_blueprint(errors)
+
+    return app
+
--- flaskblog/users/forms.py
+++ flaskblog/users/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField,FileAllowed # For Upload File
-from wtforms import StringField,PasswordField,SubmitField,BooleanField,TextAreaField
-from wtforms.validators import DataRequired,Length,Email,EqualTo,ValidationError
-from flaskblog.models import User
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_login import current_user
+from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',validators=[DataRequired(),Length(2,20)])
@@ -51,10 +51,6 @@ class UpdateAccountForm(FlaskForm):
                 raise ValidationError('That Email is already taken. Please Choose different one')
 
 
-class PostForm(FlaskForm):
-    title = StringField('Title',validators=[DataRequired()])
-    content = TextAreaField('Content',validators=[DataRequired()])
-    submit = SubmitField('Post')
 
 class RequestResetForm(FlaskForm):
     email = StringField('Email',validators=[DataRequired(),Email()])
@@ -70,4 +66,3 @@ class ResetPasswordForm(FlaskForm):
     password = PasswordField('Password',validators=[DataRequired(),Length(6,20)])
     confirm_password = PasswordField('Confirm Password',validators=[DataRequired(),EqualTo('password')])
     submit = SubmitField('Reset Password')
-
